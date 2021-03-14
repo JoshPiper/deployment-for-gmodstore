7627ae18f5af346a03a99e550d4d75c86aafa396
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2021-03-14 13:04:27 +0000

    feat(build): Automatically update the readme version during the chores. (#6)
    * Add the pre-commit hook handler.
    
    * Condense that down, it's only a 1 liner.
    
    * Add the pre-commit hook.
    
    * eof newline.

---
+++ .github/scripts/pre-commit.js
@@ -0,0 +1,16 @@
+const {join} = require("path")
+const {readdir} = require("fs").promises
+const proc = require("child_process")
+const {promisify} = require("util")
+
+const exec = promisify(proc.exec)
+
+exports.preCommit = async ({tag, version}) => {
+	process.env["BUILD_TAG"] = tag
+	process.env["BUILD_VERSION"] = version
+	let basedir = join(__dirname, 'pre-commit')
+
+	let files = await readdir(basedir)
+	files = files.map(file => join(basedir, file))
+	files.forEach(file => exec(file))
+}
