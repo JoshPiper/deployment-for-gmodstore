cf6da489774828fc148b2e039e92f43851dfb145
JoshPiper <32164094+JoshPiper@users.noreply.github.com>
2025-09-20 21:50:17 +0100

    style: fix spelling error and spacing.

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
 import {main} from "./main"
+
 main().catch(err => {
-	console.error(`An unexpected error occured.\n${err}`)
+	console.error(`An unexpected error occurred.\n${err}`)
 	process.exit(1)
 })
